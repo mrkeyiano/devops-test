b40af99d23bb13835385bd030eb35697dc5e2882
Nifemi Alpine Durin <k47sec@gmail.com>
2021-02-12 14:58:14 +0100

    add keypair dir

--- terraform-with-python-for-infrastructure-automation/start.py
+++ terraform-with-python-for-infrastructure-automation/start.py
@@ -1,5 +1,5 @@
 import os
 access_key = input("AWS Access KEY: ")
 secret_key = input("AWS Secret KEY: ")
-region = input("AWS Region: ")
+region = input("AWS Region (eu-west-1): ")
 os.system('./start-server.sh ' + access_key + " " + secret_key + " " + region)
